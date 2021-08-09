1a2e78c581dc26b2143eb3dc3aaf94fcee8c5901
TheMaximeSan <mxm.tournier@gmail.com>
2021-08-09 11:31:21 +0200

    finish

--- src/Controller/PlayerController.php
+++ src/Controller/PlayerController.php
@@ -2,9 +2,14 @@
 
 namespace App\Controller;
 
+use App\Entity\Player;
+use App\Repository\PlayerRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\SerializerInterface;
 
 /**
  * @Route("/apifoot/player")
@@ -12,12 +17,46 @@ use Symfony\Component\Routing\Annotation\Route;
 class PlayerController extends AbstractController
 {
     /**
-     * @Route("s", name="player")
+     * @Route("s", name="playerIndex")
      */
-    public function index(): Response
+    public function index(PlayerRepository $playerRepository): Response
     {
-        return $this->render('player/index.html.twig', [
-            'controller_name' => 'PlayerController',
-        ]);
+        $players = $playerRepository->findAll();
+
+        return $this->json($players);
+    }
+
+    /**
+     * @Route("/{id}", name="playerShow", requirements={"id":"\d+"})
+     */
+    public function show(Player $player): Response
+    {
+        return $this->json($player);
+    }
+
+    /**
+     * @Route("/create", name="playerCreate", methods={"POST"})
+     */
+    public function create(Request $request, EntityManagerInterface $manager, SerializerInterface $serializer): Response
+    {
+        $data = $request->getContent();
+
+        $player = $serializer->deserialize($data, Player::class, 'json');
+
+        $manager->persist($player);
+        $manager->flush();
+
+        return $this->json($player);
+    }
+
+    /**
+     * @Route("/delete/{id}", name="playerDelete", methods={"DELETE"})
+     */
+    public function delete(Player $player, EntityManagerInterface $manager): Response
+    {
+        $manager->remove($player);
+        $manager->flush();
+
+        return $this->json("REMOVE OK");
     }
 }
---
+++ src/Entity/Player.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\PlayerRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=PlayerRepository::class)
+ */
+class Player
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $firstName;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $poste;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getFirstName(): ?string
+    {
+        return $this->firstName;
+    }
+
+    public function setFirstName(string $firstName): self
+    {
+        $this->firstName = $firstName;
+
+        return $this;
+    }
+
+    public function getPoste(): ?string
+    {
+        return $this->poste;
+    }
+
+    public function setPoste(string $poste): self
+    {
+        $this->poste = $poste;
+
+        return $this;
+    }
+}
